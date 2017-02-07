b35e7ec3c7963e1e56c5b6eec7d91b7f94b81b33
Slaven Rezic <slaven@rezic.de>
2017-02-07 22:19:22 +0100

    remote sudo example

--- examples/remote.pl
+++ examples/remote.pl
@@ -10,15 +10,21 @@ use FindBin;
 use lib ("$FindBin::RealBin/../lib");
 use Doit;
 
+sub hello {
+    require Sys::Hostname;
+    warn "This runs somewhere: " . Sys::Hostname::hostname();
+}
+
 return 1 if caller;
 
 my $doit = Doit->init;
-my $remote = $doit->do_ssh_connect("cvrsnica", debug => 1);
+my $remote = $doit->do_ssh_connect("eserte.dev4.bbbike.org", debug => 1, as => "root");
 my $cmd = q{echo $(hostname; echo -n ": "; date)};
 $doit->system($cmd);
+$doit->call("hello");
 $remote->system($cmd);
-$remote->mkdir("/tmp/doit-created");
-$remote->system("ls -ald /tmp/doit*");
+$remote->system("id");
+$remote->call("hello");
 #$remote->exit;
 
 __END__
