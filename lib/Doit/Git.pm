abf10ac4b9fcdc200f24db4fadcd7d76e07716c6
Slaven Rezic <slaven@rezic.de>
2017-03-09 21:32:49 +0100

    beginnings of a git component

---
+++ lib/Doit/Git.pm
@@ -0,0 +1,144 @@
+# -*- perl -*-
+
+#
+# Author: Slaven Rezic
+#
+# Copyright (C) 2017 Slaven Rezic. All rights reserved.
+# This package is free software; you can redistribute it and/or
+# modify it under the same terms as Perl itself.
+#
+# Mail: slaven@rezic.de
+# WWW:  http://www.rezic.de/eserte/
+#
+
+package Doit::Git; # Convention: all commands here should be prefixed with 'git_'
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = '0.01';
+
+sub new { bless {}, shift }
+sub functions { qw(git_repo_update git_short_status git_root git_get_commit_hash) }
+
+sub _in_directory (&$) {
+    my($code, $dir) = @_;
+    my $save_pwd;
+    if (defined $dir) {
+	$save_pwd = save_pwd2();
+	chdir $dir or die "Can't chdir to $dir: $!";
+    }
+    $code->();
+}
+
+sub git_repo_update {
+    my($self, %opts) = @_;
+    my $repository = delete $opts{repository};
+    my $directory = delete $opts{directory};
+    my $origin = delete $opts{origin} || 'origin';
+    die "Unhandled options: " . join(" ", %opts) if %opts;
+
+    my $save_pwd = save_pwd2();
+
+    my $has_changes = 0;
+    if (-e $directory) {
+	if (!-d $directory) {
+	    die "'$directory' exists, but is not a directory\n";
+	}
+	chdir $directory
+	    or die "Can't chdir $directory: $!";
+	if (!-d ".git") {
+	    die "No .git directory found in '$directory', refusing to clone...\n";
+	}
+	chomp(my $actual_repository = `git config --get 'remote.$origin.url'`); # XXX should use something "safe"
+	if ($actual_repository ne $repository) {
+	    die "remote $origin does not point to $repository, but to $actual_repository\n";
+	}
+	$self->system(qw(git fetch));
+	my $status = $self->git_short_status;
+	if ($status eq '>') {
+	    $self->system(qw(git pull)); # XXX actually would be more efficient to do a merge or rebase, but need to figure out how git does it exactly...
+	    $has_changes = 1;
+	} # else: ahead, diverged, or something else
+    } else {
+	$self->system(qw(git clone --origin), $origin, $repository, $directory);
+	$has_changes = 1;
+    }
+    $has_changes;
+}
+
+sub git_short_status {
+    my($self, %opts) = @_;
+    my $directory = delete $opts{directory};
+    die "Unhandled options: " . join(" ", %opts) if %opts;
+
+    _in_directory {
+	local $ENV{LC_ALL} = 'C';
+	open my $fh, "-|", "git", "status", "--untracked-files=no"
+	    or die "Can't run git status: $!";
+	my $l;
+	$l = <$fh>;
+	$l = <$fh>;
+	if      ($l =~ m{^(# )?Your branch is ahead}) {
+	    return '<';
+	} elsif ($l =~ m{^(# )?Your branch is behind}) {
+	    return '>';
+	} elsif ($l =~ m{^(# )?Your branch and .* have diverged}) {
+	    return '<>';
+	} else {
+	    return '?';
+	}
+    } $directory;
+}
+
+sub git_root {
+    my($self, %opts) = @_;
+    my $directory = delete $opts{directory};
+    die "Unhandled options: " . join(" ", %opts) if %opts;
+
+    _in_directory {
+	chomp(my $dir = `git rev-parse --show-toplevel`);
+	$dir;
+    } $directory;
+}
+
+sub git_get_commit_hash {
+    my($self, %opts) = @_;
+    my $directory = delete $opts{directory};
+    die "Unhandled options: " . join(" ", %opts) if %opts;
+
+    _in_directory {
+	chomp(my $commit = `git log -1 --format=%H`);
+	$commit;
+    } $directory;
+}
+
+# REPO BEGIN
+# REPO NAME save_pwd2 /Users/eserte/src/srezic-repository 
+# REPO MD5 d0ad5c46f2276dc8aff7dd5b0a83ab3c
+
+BEGIN {
+    sub save_pwd2 {
+	require Cwd;
+	my $pwd = Cwd::getcwd();
+	if (!defined $pwd) {
+	    warn "No known current working directory";
+	}
+	bless {cwd => $pwd}, __PACKAGE__ . '::SavePwd2';
+    }
+    my $DESTROY = sub {
+	my $self = shift;
+	if (defined $self->{cwd}) {
+	    chdir $self->{cwd}
+	        or die "Can't chdir to $self->{cwd}: $!";
+	}
+    };
+    no strict 'refs';
+    *{__PACKAGE__.'::SavePwd2::DESTROY'} = $DESTROY;
+}
+# REPO END
+
+1;
+
+__END__
---
+++ t/git.t
@@ -0,0 +1,58 @@
+#!/usr/bin/perl -w
+# -*- cperl -*-
+
+#
+# Author: Slaven Rezic
+#
+
+use strict;
+use File::Temp qw(tempdir);
+use Test::More;
+
+use Doit;
+use Doit::Extcmd qw(is_in_path);
+
+if (!is_in_path('git')) {
+    plan skip_all => 'git not in PATH';
+}
+
+plan 'no_plan';
+
+my $d = Doit->init;
+$d->add_component('git');
+
+my $dir = tempdir('doit-git-XXXXXXXX', CLEANUP => 1, TMPDIR => 1);
+
+SKIP: {
+    my $self_git = $d->git_root;
+    skip "Not a git checkout", 1 if !$self_git;
+
+    my $workdir = "$dir/doit";
+
+    is $d->git_repo_update(repository => $self_git, directory => $workdir), 1, 'first call is a clone';
+    my $commit_hash = $d->git_get_commit_hash(directory => $workdir);
+    like $commit_hash, qr{^[0-9a-f]{40}$}, 'a sha1';
+    ok -d $workdir;
+    ok -d "$workdir/.git";
+    ok -f "$workdir/lib/Doit.pm";
+    is $d->git_repo_update(repository => $self_git, directory => $workdir), 0, 'second call does nothing';
+    is $d->git_get_commit_hash(directory => $workdir), $commit_hash, 'unchanged commit hash';
+
+    chdir $workdir or die $!;
+    $d->system(qw(git reset --hard HEAD^));
+
+    is $d->git_repo_update(repository => $self_git, directory => $workdir), 1, 'doing a fetch';
+    is $d->git_get_commit_hash, $commit_hash, 'again at the old commit hash'; # ... and without specifying $workdir
+
+    chdir "/"; # for File::Temp cleanup
+
+    $d->mkdir("$dir/exists");
+    eval { $d->git_repo_update(repository => $self_git, directory => "$dir/exists") };
+    like $@, qr{No .git directory found in};
+
+    $d->touch("$dir/file");
+    eval { $d->git_repo_update(repository => $self_git, directory => "$dir/file") };
+    like $@, qr{exists, but is not a directory};
+}
+
+__END__
