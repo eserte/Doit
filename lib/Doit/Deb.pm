c632a21fc16e105b93cc185ce48351f0316ddcd7
Slaven Rezic <slaven@rezic.de>
2020-05-28 19:54:06 +0200

    deb key installation: fallback to wget if curl is not available

--- lib/Doit/Deb.pm
+++ lib/Doit/Deb.pm
@@ -140,7 +140,12 @@ sub deb_install_key {
 	if ($keyserver) {
 	    $self->system(get_sudo_cmd(), 'apt-key', 'adv', '--keyserver', $keyserver, '--recv-keys', $key);
 	} elsif ($url) {
-	    my @fetch_cmd = ('curl', '-fsSL', $url);
+	    my @fetch_cmd;
+	    if (-x "/usr/bin/curl") { # XXX avoid Doit::ExtCmd::is_in_path until remote sync issues with nested components are resolved
+		@fetch_cmd = ('curl', '-fsSL', $url);
+	    } else {
+		@fetch_cmd = ('wget', '-O-', $url); # other alternative would be lwp-request
+	    }
 	    my @add_cmd   = (get_sudo_cmd(), 'apt-key', 'add', '-');
 	    if ($self->is_dry_run) {
 		info "Fetch key using '@fetch_cmd' and add using '@add_cmd' (dry-run)";
